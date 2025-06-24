a0fbf658f84de62c7a94ef5bcbdeaa5a9ee61e11
Brandon Hsieh <brandonhsieh09@gmail.com>
2025-06-24 16:29:33 -0400

    Fix PDF path and update site

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/ap-helper/', // Correct base for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
