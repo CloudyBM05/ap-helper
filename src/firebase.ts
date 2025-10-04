6fb58ee9392095cf643a14ab7518b2a2a69fb7dd
Brandon Hsieh <brandonhsieh09@gmail.com>
2025-10-04 12:12:25 -0400

    Fix Firebase project ID to match service account

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,9 +3,9 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpj-qvtUOKMGRcvYlWgO_jg-U8O9VV8k8",
-  authDomain: "ap-helper-app.firebaseapp.com",
-  projectId: "ap-helper-app",
-  storageBucket: "ap-helper-app.firebasestorage.app",
+  authDomain: "ap-helper-7a6ac.firebaseapp.com",
+  projectId: "ap-helper-7a6ac",
+  storageBucket: "ap-helper-7a6ac.firebasestorage.app",
   messagingSenderId: "474747866896",
   appId: "1:474747866896:web:a3f2e9c0b0b1e2f3g4h5i6"
 };
